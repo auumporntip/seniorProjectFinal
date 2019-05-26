df3f7a925ad4f9a75393e5e80fd69f1cf5ff07bc
moshirosang <tiw_tisadee@hotmail.com>
2019-05-27 01:54:00 +0700

    [Change Port]

--- server/src/index.js
+++ server/src/index.js
@@ -25,7 +25,7 @@ app.use('/api/',require('./routes/transaction'))
 app.use('/api/',require('./upload/upload'))
 
 
-app.listen(7777, () => {
+app.listen(8443, () => {
   console.log('Example app listening at http://%s:%s');
 })
 
