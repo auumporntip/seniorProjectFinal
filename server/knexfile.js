65f059db056d6f0fb442ad11f5889dc402cb7ef8
moshirosang <tiw_tisadee@hotmail.com>
2019-09-06 16:46:51 +0700

    [UPDATE]Position

--- server/knexfile.js
+++ server/knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
         connection: {
             host: 'localhost',
             user: 'root',
-            password: '',
+            password: '123456789',
             database: 'RestaurantManagement'
         }
     }
