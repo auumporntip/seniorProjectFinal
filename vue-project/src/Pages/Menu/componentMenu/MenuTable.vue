2fdfb9556e7aa8c7e7ed2d50a756226bfdc96a10
auumporntip <a_auum@hotmail.com>
2019-09-22 21:17:48 +0700

    [update] add category

--- vue-project/src/Pages/Menu/componentMenu/MenuTable.vue
+++ vue-project/src/Pages/Menu/componentMenu/MenuTable.vue
@@ -2,106 +2,103 @@
   <div>
     <span id="test">
       <v-flex xs12>
-      <v-card-title>
-      Category of food
-      
-    <!-- <span class="head">Category of Food:</span> -->
-    <b-dropdown v-model="selectedCategory">
-      
-      <button class="button is-dark" slot="trigger" v-if="this.$store.getters.checkCategory">
-        {{selectedCategory.categoryName}}
-        <b-icon icon="menu-down"></b-icon>
-      </button>
-      <button class="button is-dark" slot="trigger" v-else id="button">
-        All Category
-        <b-icon icon="menu-down"></b-icon>
-      </button>
-      <b-dropdown-item @click="allCategory">All Category</b-dropdown-item>
-      <b-dropdown-item
-        v-for="option in category"
-        :value="option"
-        :key="option.categoryId"
-        @click="changeCategoryMenu"
-      >{{option.categoryName}}</b-dropdown-item>
-    <v-layout>
-      <v-flex xs2>
-      <v-btn  class="add" @click="AddDialog=true">Add Category</v-btn>
-      <v-dialog v-model="AddDialog" max-width="490">
-        <v-card>
-          <v-card-text class="headline">
-            Add Category
-            <v-form ref="form">
-              <v-container fluid>
-                <v-text-field
-                label="categoryName"
-                v-model="newCat.categoryName"
-                type="text"
-                :rules="nameRules">
-                </v-text-field>
-              </v-container>
-            </v-form>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn  text @click="addCancel">Cancel</v-btn>
-            <v-btn  text @click="addSave">Save</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
+        <v-card-title>
+          Category of food
+          <!-- <span class="head">Category of Food:</span> -->
+          <b-dropdown v-model="selectedCategory">
+            <button class="button is-dark" slot="trigger" v-if="this.$store.getters.checkCategory">
+              {{selectedCategory.categoryName}}
+              <b-icon icon="menu-down"></b-icon>
+            </button>
+            <button class="button is-dark" slot="trigger" v-else id="button">
+              All Category
+              <b-icon icon="menu-down"></b-icon>
+            </button>
+            <b-dropdown-item @click="allCategory">All Category</b-dropdown-item>
+            <b-dropdown-item
+              v-for="option in category"
+              :value="option"
+              :key="option.categoryId"
+              @click="changeCategoryMenu"
+            >{{option.categoryName}}</b-dropdown-item>
+            <v-layout>
+              <v-flex xs2>
+                <v-btn class="add" @click="AddDialog=true">Add Category</v-btn>
+                <v-dialog v-model="AddDialog" max-width="490">
+                  <v-card>
+                    <v-card-text class="headline">
+                      Add Category
+                      <v-form ref="form">
+                        <v-container fluid>
+                          <v-text-field
+                            label="categoryName"
+                            v-model="newCat.categoryName"
+                            type="text"
+                            :rules="[rules.nameRules,rules.check]"
+                          ></v-text-field>
+                        </v-container>
+                      </v-form>
+                    </v-card-text>
+                    <v-card-actions>
+                      <v-spacer></v-spacer>
+                      <v-btn text @click="addCancel">Cancel</v-btn>
+                      <v-btn text @click="addSave">Save</v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-dialog>
+              </v-flex>
+            </v-layout>
+          </b-dropdown>
+          <v-text-field
+            class="search"
+            v-model="keyword"
+            append-icon="search"
+            label="Search"
+            single-line
+            hide-details
+          ></v-text-field>
+        </v-card-title>
       </v-flex>
-    </v-layout>
-     
-    </b-dropdown>
-    <v-text-field
-        class="search"
-        v-model="keyword"
-        append-icon="search"
-        label="Search"
-        single-line
-        hide-details
-      ></v-text-field>
-    </v-card-title></v-flex></span>
+    </span>
 
     <!-- <div class="search"> -->
-      <!-- <b-field>
+    <!-- <b-field>
         <b-input placeholder="Search..." type="search" v-model="keyword"></b-input>
-      </b-field> -->
-         
-      
-      <b-table
-        :data="items"
-        :selected.sync="selectedMenu"
-        focusable
-        class="elevation-1"
-        :paginated="isPaginated"
-        :per-page="perPage"
-        :current-page.sync="currentPage"
-        :pagination-simple="isPaginationSimple"
-        aria-next-label="Next page"
-        aria-previous-label="Previous page"
-        aria-page-label="Page"
-        aria-current-label="Current page"
-        @select="selected"
-      >
-        <template slot-scope="props">
-          <b-table-column label="Image" width="80">
-            <div class="img-resize" v-if="props.row.menuPathImage!=null">
-              <img :src="props.row.menuPathImage" />
-            </div>
-            <div class="img-resize" v-else>
-              <img src="../../../assets/1.png" />
-            </div>
-          </b-table-column>
+    </b-field>-->
 
-          <b-table-column label="Menu Name" width="200">{{ props.row.menuName }}</b-table-column>
+    <b-table
+      :data="items"
+      :selected.sync="selectedMenu"
+      focusable
+      class="elevation-1"
+      :paginated="isPaginated"
+      :per-page="perPage"
+      :current-page.sync="currentPage"
+      :pagination-simple="isPaginationSimple"
+      aria-next-label="Next page"
+      aria-previous-label="Previous page"
+      aria-page-label="Page"
+      aria-current-label="Current page"
+      @select="selected"
+    >
+      <template slot-scope="props">
+        <b-table-column label="Image" width="80">
+          <div class="img-resize" v-if="props.row.menuPathImage!=null">
+            <img :src="props.row.menuPathImage" />
+          </div>
+          <div class="img-resize" v-else>
+            <img src="../../../assets/1.png" />
+          </div>
+        </b-table-column>
 
-          <b-table-column label="Price" width="200">{{ props.row.menuPrice }}</b-table-column>
+        <b-table-column label="Menu Name" width="200">{{ props.row.menuName }}</b-table-column>
 
-          <b-table-column label="Category" width="0">{{ props.row.categoryName }}</b-table-column>
-        </template>
-      </b-table>
-    
-    </div>
+        <b-table-column label="Price" width="200">{{ props.row.menuPrice }}</b-table-column>
+
+        <b-table-column label="Category" width="0">{{ props.row.categoryName }}</b-table-column>
+      </template>
+    </b-table>
+  </div>
   <!-- </div> -->
 </template>
 <script>
@@ -115,7 +112,9 @@ export default {
       //Add
       AddDialog: false,
       newCat: [],
-      nameRules: [v => !!v || "Name is required"],
+      rules: {
+        nameRules: v => !!v || "Name is required",
+      },
       isPaginated: true,
       isPaginationSimple: false,
       currentPage: 1,
@@ -129,31 +128,42 @@ export default {
     };
   },
   methods: {
-    addCancel(){
-      this.AddDialog=false;
-      this.$refs.form.resetValidation();
-      this.newCat= [];
+    check(){
+      for (let index = 0; index < this.category.length; index++) {
+            if (
+              this.newCat.categoryName.toLowerCase() ===
+              this.category[index].categoryName.toLowerCase()
+            ) {
+              return false;
+            }
+          }
+          this.$refs.form.rules
+      return true;
     },
-    addSave(){
-      if(this.$refs.form.validate()){
+    addCancel() {
+      this.AddDialog = false;
+      this.$refs.form.resetValidation();
+      this.newCat = [];
+    },
+    addSave() {
+      if (this.$refs.form.validate() && this.check()) {
         console.log(this.newCat.categoryName);
         axios
-        .post("http://localhost:3000/api/insertCategory",{
-          categoryName: this.newCat.categoryName,
-          restaurantId: 1
-        })
-        .then(response => {
-          this.reCat();
-          this.newCat = [],
-          this.$refs.form.resetValidation();
-        });
-        this.AddDialog=false;
+          .post("http://localhost:3000/api/insertCategory", {
+            categoryName: this.newCat.categoryName,
+            restaurantId: 1
+          })
+          .then(response => {
+            this.reCat();
+            (this.newCat = []), this.$refs.form.resetValidation();
+          });
+        this.AddDialog = false;
       }
     },
-    reCat(){
+    reCat() {
       axios.get("http://localhost:3000/api/getallcategory").then(response => {
         this.category = response.data;
-      })
+      });
     },
     changeCategoryMenu() {
       axios
@@ -179,8 +189,7 @@ export default {
         this.$store.commit("setSelectedMenu", this.selectedMenu);
         console.log(this.selectedMenu);
       }, 1);
-    },
-   
+    }
   },
   computed: {
     items() {
@@ -218,12 +227,10 @@ export default {
   margin-left: 20px;
 }
 .search {
-  margin-left: 800px;
+  margin-left: 57%;
   position: absolute;
-
 }
 #button {
   margin-left: 10px;
 }
-
 </style>
